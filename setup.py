b751b420f43f7079887a071b1aa976c84d7f2a17
Ismael Benito <isman7@gmail.com>
2017-01-02 00:26:00 +0100

    setup.py with distutils added.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Dashboardpy',
+    version='0.0.2',
+    packages=['dashboard'],
+    url='https://github.com/isman7/Dashboardpy',
+    license='MIT',
+    author='Ismael Benito',
+    author_email='isman7@gmail.com',
+    description='Dashboardpy is an Bottlepy-based dashboard for Python!'
+)
