7bcb83c2f58ecf466d3aac6fdfeda510411c5520
ismansiete <isman7@gmail.com>
2017-01-03 21:49:42 +0100

    some fixes in package distribution.

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -3,5 +3,5 @@ import bottle
 import logging
 import os
 
-bottle.TEMPLATE_PATH.append(os.path.join(os.path.dirname(__file__), 'views'))
+bottle.TEMPLATE_PATH.append(os.path.join(os.path.dirname(__file__), 'views/'))
 logging.info(bottle.TEMPLATE_PATH)
--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from distutils.core import setup
+import os
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files_1 = package_files('dashboard/static/')
+extra_files_2 = package_files('dashboard/views/')
+
+extra_files = extra_files_1 + extra_files_2
 
 setup(
     name='Dashboardpy',
-    version='0.0.2',
+    version='0.0.5',
     packages=['dashboard'],
+    package_data={'dashboard': extra_files},
     url='https://github.com/isman7/Dashboardpy',
     license='MIT',
     author='Ismael Benito',
